fbeb87120909c76789f423ab467f8aba8eb09b81
sandond77 <sandu@ucdavis.edu>
2017-12-03 20:27:23 -0800

    gif organization on html fixed

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -13,6 +13,10 @@ makeButtons();
 $(document).on("click", ".gifButton", function addGifs(){
 	$(".insert").empty(); //emptys the div of any previous gifs	
 
+	for (var i = 0; i < 10; i++) {
+		$("#gif"+i).empty();
+	}
+	
 	subject = $(this).attr("data-name");
 
 	queryURL = "http://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g";
@@ -39,7 +43,8 @@ $(document).on("click", ".gifButton", function addGifs(){
 			$(gif).attr("status", "still");
 			$(gif).attr("class", "gif");
 			$(p).prepend(rating);
-			$(".insert").append(p)
+			// $(".insert").append(p)
+			$("#gif"+i).append(p)
 		}
 	})
 })
